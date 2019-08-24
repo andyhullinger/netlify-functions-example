3014f47449994ecd704f2c214f1d694f632814de
Andrew Hullinger <ahullinger@hyconnect.com>
2019-08-24 11:30:50 -0500

    added ck fetch purchaseorder fetch

---
+++ src/lambda/ck_fetch.js
@@ -0,0 +1,13 @@
+import fetch from "node-fetch";
+
+const API_ENDPOINT = "http://purchaseorderprocessproxy.us-e2.cloudhub.io/api/PurchaseOrder";
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: data
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
